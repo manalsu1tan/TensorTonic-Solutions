c979400e3910d1031ecb0a54f4faaa3c7d5d2471
manalsu1tan <63901870+manalsu1tan@users.noreply.github.com>
2026-03-31 21:40:20 -0700

    Update solution for percentiles

--- percentiles/percentiles.py
+++ percentiles/percentiles.py
@@ -4,24 +4,20 @@ def percentiles(x, q):
     """
     Compute percentiles using linear interpolation.
     """
-    new_x = np.array(x)
-    new_x = np.sort(new_x)
-    new_q = np.array(q)
-    new_q = np.sort(new_q)
+    new_x = np.sort(np.array(x))
+    new_q = np.sort(np.array(q))
     arr = []
-    for i in q:
+    for i in new_q:
         if i == 0:
             arr.append(new_x[0])
         elif i == 100:
             arr.append(new_x[-1])
         else:
             idx = (len(new_x) - 1) * (i / 100)
-            v_floor = np.floor(idx).astype(int) 
-            v_ceil = np.ceil(idx).astype(int)
-            lower = new_x[v_floor]
-            upper = new_x[v_ceil]
+            v_floor = int(np.floor(idx))
+            v_ceil = int(np.ceil(idx))
             weight = idx % 1
-            arr.append((1 - weight) * lower + weight * upper)
+            arr.append((1 - weight) * new_x[v_floor] + weight * new_x[v_ceil])
     return np.array(arr)
             
             
