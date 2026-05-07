fc7c482392699a9957dd3436d9a061957b21e5d5
Manal Sultan <63901870+manalsu1tan@users.noreply.github.com>
2026-05-06 23:02:31 -0700

    Add solution for pandas-setting-index

---
+++ study-plans/pandas-basics/pandas-setting-index/pandas-setting-index.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def set_index_column(data, index_col):
+    """
+    Returns: dict with 'index_values', 'columns', 'data'
+    """
+    df = pd.DataFrame(data)
+    df = df.set_index(index_col)
+    return {
+        "index_values": df.index.tolist(),
+        "columns": list(df.columns),
+        "data": df.to_dict(orient="list") 
+    }
+    
