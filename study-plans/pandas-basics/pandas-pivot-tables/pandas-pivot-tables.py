0ac05d90a0debf450992859e85cf3a45e7279a92
Manal Sultan <63901870+manalsu1tan@users.noreply.github.com>
2026-07-07 14:02:17 -0700

    Solve pandas-pivot-tables

---
+++ study-plans/pandas-basics/pandas-pivot-tables/pandas-pivot-tables.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def create_pivot(data, index, columns, values, aggfunc):
+    """
+    Returns: nested dict {column_value: {index_value: agg_result}}
+    """
+    df = pd.DataFrame(data)
+    return df.pivot_table(index=index, columns=columns, values=values, aggfunc=aggfunc, fill_value=0).to_dict()
